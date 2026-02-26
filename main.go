60a94e5918c9d084448840302ac555dfdc0279a4
rezafauzan <rezafauzan593@gmail.com>
2026-02-26 11:55:48 +0700

    Feat: add new route POST /users get user payload

--- main.go
+++ main.go
@@ -13,11 +13,30 @@ var users []User
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/users", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"messages": "success",
 			"users":    users,
 		})
 	})
+
+	r.POST("/users", func(ctx *gin.Context) {
+		data := User{}
+		err := ctx.ShouldBindJSON(&data)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"Success":  false,
+				"Messages": "Failed to create users",
+			})
+		} else {
+			ctx.JSON(200, gin.H{
+				"Success":  true,
+				"Messages": "Users created",
+				"User": data,
+			})
+		}
+	})
+
 	r.Run("localhost:8888")
 }
