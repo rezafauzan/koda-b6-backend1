d3a870eb77337d79d436915715b8b66732d3c2e1
rezafauzan <rezafauzan593@gmail.com>
2026-02-26 11:30:49 +0700

    Feat: add new route GET /users to show all users

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main(){
-	
+import "github.com/gin-gonic/gin"
+
+type User struct {
+	Id       int
+	Fullname string
+	Email    string
+	Password string
+}
+
+var users []User
+
+func main() {
+	r := gin.Default()
+	r.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"messages": "success",
+			"users":    users,
+		})
+	})
+	r.Run("localhost:8888")
 }
