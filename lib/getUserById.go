5d89e20048c52d1c02f425914e90588c26a387a7
rezafauzan <rezafauzan593@gmail.com>
2026-03-02 08:33:02 +0700

    Feat: Separate getUserById function in to package lib

---
+++ lib/getUserById.go
@@ -0,0 +1,30 @@
+package lib
+
+import (
+	"github.com/gin-gonic/gin"
+	"rezafauzan/koda-b6-backend1/data"
+	"strconv"
+)
+
+func GetUserById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	userFoundId := -1
+	for x := range data.Users {
+		if strconv.Itoa(data.Users[x].Id) == id {
+			userFoundId = x
+		}
+	}
+	if userFoundId > -1 {
+		ctx.JSON(200, data.Response{
+			Success:      true,
+			Messages:     "User found!",
+			ResponseBody: data.Users[userFoundId],
+		})
+	} else {
+		ctx.JSON(400, data.Response{
+			Success:      false,
+			Messages:     "User not found !",
+			ResponseBody: "",
+		})
+	}
+}
