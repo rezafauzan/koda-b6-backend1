c92e938474fad3e44e01e9f7a28b9f12f1cc5deb
rezafauzan <rezafauzan593@gmail.com>
2026-03-06 19:16:52 +0700

    Feat: add user model contain struct of user

---
+++ internal/models/user.model.go
@@ -0,0 +1,13 @@
+package models
+
+type UserStruct struct {
+	Id        int
+	Avatar    string
+	Firstname string
+	Lastname  string
+	Email     string
+	Phone     string
+	Address   string
+	Password  string
+	Role      string
+}
