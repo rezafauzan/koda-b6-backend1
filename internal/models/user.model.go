17a9754c45c8276cd789b8b2d3673f7ba9bb3cef
rezafauzan <rezafauzan593@gmail.com>
2026-03-09 10:58:21 +0700

    Feat: add CreateUserRequest DTO

--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -11,3 +11,12 @@ type User struct {
 	Password  string `json:"password"`
 	Role      string `json:"role"`
 }
+
+type CreateUserRequest struct{
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+	Phone     string `json:"phone"`
+	Address   string `json:"address"`
+	Password  string `json:"password"`
+}
