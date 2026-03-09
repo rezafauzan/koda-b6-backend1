e92c980a5ce8d81c0a14c514cd8fed3218141849
rezafauzan <rezafauzan593@gmail.com>
2026-03-09 11:11:40 +0700

    Feat: add product model

---
+++ internal/models/product.model.go
@@ -0,0 +1,12 @@
+package models
+
+type Product struct {
+	Id              int     `json:"id"`
+	CategoryId      int     `json:"category_id"`
+	FavoriteProduct bool    `json:"favorite_product"`
+	Name            string  `json:"name"`
+	Description     string  `json:"description"`
+	Price           float64 `json:"price"`
+	CampaignId      int     `json:"campaign_id"`
+	Stock           string  `json:"stock"`
+}
