208d1f2caff556323a3b7ce4c15a0ee5522b37de
rezafauzan <rezafauzan593@gmail.com>
2026-03-09 11:12:52 +0700

    Feat: add product CRUD DTO

--- internal/models/product.model.go
+++ internal/models/product.model.go
@@ -10,3 +10,39 @@ type Product struct {
 	CampaignId      int     `json:"campaign_id"`
 	Stock           string  `json:"stock"`
 }
+
+type CreateProductRequest struct {
+	CategoryId      int     `json:"category_id"`
+	FavoriteProduct bool    `json:"favorite_product"`
+	Name            string  `json:"name"`
+	Description     string  `json:"description"`
+	Price           float64 `json:"price"`
+	CampaignId      int     `json:"campaign_id"`
+	Stock           string  `json:"stock"`
+}
+
+type GetProductResponse struct {
+	Id              int     `json:"id"`
+	CategoryId      int     `json:"category_id"`
+	FavoriteProduct bool    `json:"favorite_product"`
+	Name            string  `json:"name"`
+	Description     string  `json:"description"`
+	Price           float64 `json:"price"`
+	CampaignId      int     `json:"campaign_id"`
+	Stock           string  `json:"stock"`
+}
+
+type UpdateProductRequest struct {
+	Id              int     `json:"id"`
+	CategoryId      int     `json:"category_id"`
+	FavoriteProduct bool    `json:"favorite_product"`
+	Name            string  `json:"name"`
+	Description     string  `json:"description"`
+	Price           float64 `json:"price"`
+	CampaignId      int     `json:"campaign_id"`
+	Stock           string  `json:"stock"`
+}
+
+type DeleteProductRequest struct {
+	Id int `json:"id"`
+}
